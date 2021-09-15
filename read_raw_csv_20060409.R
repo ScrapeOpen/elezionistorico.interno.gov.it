b534adc361b9c598947c36b76f2d1fdd7f9028d0
fraba <me@francescobailo.net>
2021-09-16 08:34:50 +1000

    Major: Senate dropped, added geo_id

--- read_raw_csv_20060409.R
+++ read_raw_csv_20060409.R
@@ -1,158 +1,198 @@
-setwd('~/public_git/ScrapeOpen/elezionistorico.interno.gov.it') # Change this!
-options(warn=2) # Warnings into errors!
+library(tidyverse)
+library(sf)
+library(stringi)
 
-dir <- "~/Downloads/raw_csv_20060409/" # Change this
-files <- list.files(dir)
-files <- unique(files)
+camera_italia.20060409 <- 
+  rbind(read.csv("raw_csv_20060409/camera_italia-20060409.txt", sep=";",
+                 encoding = "latin1") %>% 
+        dplyr::mutate(VOTI_LISTA = VOTILISTA) %>%
+          dplyr::select(-VOTILISTA),
+        read.csv("raw_csv_20060409/camera_vaosta-20060409.txt", sep=";",
+                 encoding = "latin1") %>%
+          dplyr::mutate(CIRCOSCRIZIONE = "VALLE D'AOSTA",
+                        PROVINCIA = "VALLE D'AOSTA",
+                        ELETTORI = ELETTORI_TOTALI,
+                        VOTANTI = VOTANTI_TOTALI) %>%
+          dplyr::select(CIRCOSCRIZIONE, PROVINCIA, COMUNE, ELETTORI, ELETTORI_MASCHI,
+                        VOTANTI, VOTANTI_MASCHI, SCHEDE_BIANCHE, LISTA, VOTI_LISTA))
 
-# Check files
-library(stringr)
-filename_head <- str_extract(files, "^[A-Z]+-[A-Z][a-z]+")
-table(filename_head)
-## Missing
-files_camera <- files[grepl("Camera", files)]
-files_camera_scrutini <- gsub("SCRUTINI-", "", files_camera[grepl("SCRUTINI", files_camera)])
-files_camera_liste <- gsub("LISTE-", "", files_camera[grepl("LISTE", files_camera)])
-files_camera_scrutini[!files_camera_scrutini %in% files_camera_liste]
+comuni2006.sf <-
+  read_sf("raw_shp_20060413/Com01012006_WGS84.shp")
 
-files_senato <- files[grepl("Senato", files)]
-files_senato_scrutini <- gsub("SCRUTINI-", "", files_senato[grepl("SCRUTINI", files_senato)])
-files_senato_liste <- gsub("LISTE-", "", files_senato[grepl("LISTE", files_senato)])
-files_senato_scrutini[! files_senato_scrutini %in% files_senato_liste]
+comune_minint_2006 <- 
+  data.frame(
+    provincia = camera_italia.20060409$PROVINCIA,
+    original = camera_italia.20060409$COMUNE) %>%
+  dplyr::distinct(provincia, original) %>%
+  dplyr::mutate(clean = 
+                  stri_trans_general(str = tolower(gsub("'$", "", gsub("' |-|' ", " ", original))), 
+                                     id = "Latin-ASCII"))
+comune_istat_2006 <- 
+  data.frame(
+    PRO_COM_T = comuni2006.sf$PRO_COM_T,
+    original = comuni2006.sf$COMUNE) %>%
+  dplyr::distinct(PRO_COM_T, original) %>%
+  dplyr::mutate(clean = 
+                  stri_trans_general(str = tolower(gsub("'$", "", gsub("' |-|' ", " ", original))), 
+                                     id = "Latin-ASCII"))  
 
-files <- list.files(dir, full.names = F)
+comune_minint_2006$clean[comune_minint_2006$clean == "zeme lomellina"] <- "zeme" 
+comune_minint_2006$clean[comune_minint_2006$clean == "sannicandro garganico"] <- "san nicandro garganico"
+comune_minint_2006$clean[comune_minint_2006$clean == "cerrina"] <- "cerrina monferrato"
+comune_minint_2006$clean[comune_minint_2006$clean == "cerreto langhe"] <- "cerretto langhe"
+comune_minint_2006$clean[comune_minint_2006$clean == "cassano all ionio"] <- "cassano all'ionio"
+comune_minint_2006$clean[comune_minint_2006$clean == "viale d'asti"] <- "viale" 
+comune_minint_2006$clean[comune_minint_2006$clean == "monte grimano"] <- "monte grimano terme"
+comune_minint_2006$clean[comune_minint_2006$clean == "roncegno"] <- "roncegno terme" 
+comune_minint_2006$clean[comune_minint_2006$clean == "ruffre' mendola"] <- "ruffre mendola"
+comune_minint_2006$clean[comune_minint_2006$clean == "montecompatri"] <- "monte compatri" 
+comune_minint_2006$clean[comune_minint_2006$clean == "montebello ionico"] <- "montebello jonico" 
+comune_minint_2006$clean[comune_minint_2006$clean == "santo stino di livenza"] <- "san stino di livenza"
+comune_minint_2006$clean[comune_minint_2006$clean == "puegnago del garda"] <- "puegnago sul garda" 
+comune_minint_2006$clean[comune_minint_2006$clean == "monticello"] <- "monticello brianza"
+comune_minint_2006$clean[comune_minint_2006$clean == "santo stino di livenza"] <- "san stino di livenza" 
+comune_minint_2006$clean[comune_minint_2006$clean == "massafiscaglia"] <- "massa fiscaglia"
+comune_minint_2006$clean[comune_minint_2006$clean == "iolanda di savoia"] <- "jolanda di savoia" 
+
+comune_minint_2006$clean[comune_minint_2006$clean == "ro ferrarese"] <- "ro" 
+comune_minint_2006$clean[comune_minint_2006$clean == "baiardo"] <- "bajardo"
+comune_minint_2006$clean[comune_minint_2006$clean == "cosio di arroscia"] <- "cosio d'arroscia"
+comune_minint_2006$clean[comune_minint_2006$clean == "san remo"] <- "sanremo"
+
+comune_minint_2006$clean[comune_minint_2006$clean == "lonato"] <- "lonato del garda" 
+comune_minint_2006$clean[comune_minint_2006$clean == "pre' saint didier"] <- "pre saint didier" 
+comune_minint_2006$clean[comune_minint_2006$clean == "san dorligo della valle"] <- "san dorligo della valle dolina" 
+
+comune_minint_2006$clean[comune_minint_2006$clean == "cerreto delle langhe"] <- "cerretto langhe" 
+comune_minint_2006$clean[comune_minint_2006$clean == "nughedu  san nicolo"] <- "nughedu san nicolo" 
+
+comune_minint_2006$clean[comune_minint_2006$clean == "reana del roiale"] <- "reana del rojale" 
+comune_minint_2006$clean[comune_minint_2006$clean == "ruffre"] <- "ruffre mendola" 
+
+comune_minint_2006$clean[comune_minint_2006$clean == "massalubrense"] <- "massa lubrense" 
+comune_minint_2006$clean[comune_minint_2006$clean == "nocera tirinese"] <- "nocera terinese" 
+comune_minint_2006$clean[comune_minint_2006$clean == "poiana maggiore"] <- "pojana maggiore" 
+comune_minint_2006$clean[comune_minint_2006$clean == "lonato del garda"] <- "lonato" 
+comune_minint_2006$clean[comune_minint_2006$clean == "serra de'conti"] <- "serra de conti" 
+comune_minint_2006$clean[comune_minint_2006$clean == "torella de lombardi"] <- "torella dei lombardi" 
+comune_minint_2006$clean[comune_minint_2006$clean == "costa di serina"] <- "costa serina" 
+comune_minint_2006$clean[comune_minint_2006$clean == "monte castello"] <- "montecastello" 
+comune_minint_2006$clean[comune_minint_2006$clean == "aquila di arroscia"] <- "aquila d'arroscia" 
+comune_minint_2006$clean[comune_minint_2006$clean == "montalbano ionico"] <- "montalbano jonico" 
+comune_minint_2006$clean[comune_minint_2006$clean == "castel mola"] <- "castelmola" 
+comune_minint_2006$clean[comune_minint_2006$clean == "monaciilioni"] <- "monacilioni" 
+comune_minint_2006$clean[comune_minint_2006$clean == "colledanchise"] <- "colle d'anchise" 
+comune_minint_2006$clean[comune_minint_2006$clean == "boiano"] <- "bojano" 
+comune_minint_2006$clean[comune_minint_2006$clean == "castronuovo di sicilia"] <- "castronovo di sicilia" 
+comune_minint_2006$clean[comune_minint_2006$clean == "serra s. abbondio"] <- "serra sant'abbondio" 
+comune_minint_2006$clean[comune_minint_2006$clean == "montegrimano"] <- "monte grimano terme" 
+comune_minint_2006$clean[comune_minint_2006$clean == "santa vittoria in matemano"] <- "santa vittoria in matemano" 
+comune_minint_2006$clean[comune_minint_2006$clean == "truggio"] <- "triuggio" 
+comune_minint_2006$clean[comune_minint_2006$clean == "santa teresa di gallura"] <- "santa teresa gallura" 
+
+comune_minint_2006$clean[comune_minint_2006$clean == "gonnasfanadiga"] <- "gonnosfanadiga" 
+
+comune_minint_2006$clean[comune_minint_2006$clean == "buia"] <- "buja" 
+
+# comune_minint_2006$clean[comune_minint_2006$clean == "calliano tn"] <- ???
+# comune_minint_2006$clean[comune_minint_2006$clean == "valverde ct"] <- ???
+# comune_minint_2006$clean[comune_minint_2006$clean == "samone tn"] <- ???
+# comune_minint_2006$clean[comune_minint_2006$clean == "livo tn"] <- ???
+
+comune_minint_2006$clean[comune_minint_2006$clean == "santa vittoria in matemano"] <- 
+  "santa vittoria in matenano" 
+
+comune_minint_2006$clean[!comune_minint_2006$clean %in%
+                           comune_istat_2006$clean]
+
+comune_istat_2006$clean[!comune_istat_2006$clean
+                        %in% comune_minint_2006$clean]
+
+comune_minint_2006$PRO_COM_T <- 
+  comune_istat_2006$PRO_COM_T[match(comune_minint_2006$clean,
+                                    comune_istat_2006$clean)]
 
 
-file_path_regex <-
-  "^.*(SCRUTINI|LISTE)-(Senato|Camera)-09_04_2006-(VALLE_D'AOSTA_e_TRENTINO-ALTO_ADIGE|ITALIA_\\(escl\\._Valle_d'Aosta\\))-"
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "TRENTO" &
+                               comune_minint_2006$original == "LIVO TN"] <- "022106"
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "TRENTO" &
+                               comune_minint_2006$original == "SAMONE TN"] <- "022165"
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "CATANIA" &
+                               comune_minint_2006$original == "VALVERDE CT"] <- "087052"
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "TRENTO" &
+                               comune_minint_2006$original == "CALLIANO TN"] <- "022035"
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "BRESCIA" &
+                               comune_minint_2006$original == "BRIONE"] <- "017030"
 
-splitFileName <- function(x) {
-  require(stringr)
-  x <- gsub("FRIULI-VENEZIA", "FRIULI~VENEZIA", x)
-  x <- gsub("EMILIA-ROMAGNA", "EMILIA~ROMAGNA", x)
-  x <- gsub("TRENTINO-ALTO", "TRENTINO~ALTO", x)
-  x <- gsub("Trentino-Alto", "Trentino~Alto", x)
-  this_split <- strsplit(gsub("\\.csv", "", x), "-")[[1]]
-  this_type <- this_split[1]
-  this_what <- this_split[2]
-  this_date <- as.character(as.Date(this_split[3], "%d_%m_%Y"))
-  this_where_1 <-  this_split[5]
-  this_where_2 <- this_split[6]
-  return(list(type = this_type,
-              election = this_what,
-              date = this_date,
-              geo_lev_1 = gsub("([A-Z])~([A-Z])", "\\1-\\2", this_where_1),
-              geo_lev_2 = gsub("([A-Z])~([A-Z])", "\\1-\\2", this_where_2)))
-}
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "TRENTO" &
+                               comune_minint_2006$original == "CAGNO'"] <- "022030"
 
-makeDF <- function(lst, dat) {
-  details <-
-    data.frame(matrix(unlist(lst), nrow=1, byrow=T,
-                      dimnames = list(NULL, names(lst))))
-  details <-
-    do.call("rbind", replicate(nrow(dat), details, simplify = FALSE))
-  return(cbind(details, dat))
-}
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "LECCE" &
+                               comune_minint_2006$original == "CASTRO"] <- "075096"
 
-read_dat <- function(f) {
-  print(f)
-  require(zoo)
-  file_details <- splitFileName(f)
-  this_dat <- read.csv(paste0(dir, f), sep = ";", row.names = NULL, colClasses = 'character')
-  if (file_details[['type']] == 'SCRUTINI') {
-    this_dat <- this_dat[,1:5]
-    colnames(this_dat) <-
-      c("Ente", "Numero.elettori", "Numero.votanti", "Schede.bianche",
-        "Schede.non.valide")
-    this_dat$Numero.elettori <- as.numeric(gsub("\\.", "", this_dat$Numero.elettori))
-    this_dat$Numero.votanti <- as.numeric(gsub("\\.", "", this_dat$Numero.votanti))
-    this_dat$Schede.bianche <- as.numeric(gsub("\\.", "", this_dat$Schede.bianche))
-    this_dat$Schede.non.valide <- as.numeric(gsub("\\.", "", this_dat$Schede.non.valide))
-    return(makeDF(file_details,
-                   data.frame(geo_entity = this_dat$Ente,
-                              registered_voters = this_dat$Numero.elettori,
-                              effective_voters = this_dat$Numero.votanti,
-                              blank_votes = this_dat$Schede.bianche,
-                              invalid_votes = this_dat$Schede.non.valide,
-                              stringsAsFactors = F)))
-  } else if (file_details[['type']] == 'LISTE') {
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "CATANIA" &
+                               comune_minint_2006$original == "PATERNO'"] <- "087033"
 
-    if (file_details$geo_lev_2 == "Comune") {
-      file_details$geo_lev_2 <- file_details$geo_lev_1
-      this_dat <- this_dat[,1:4]
-      colnames(this_dat) <- c("Ente","Candidato","Voti","Liste.Gruppi")
-      this_dat <- this_dat[,c("Ente", "Candidato", "Liste.Gruppi", "Voti")]
-      }
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "COMO" &
+                               comune_minint_2006$original == "PEGLIO"] <- "013178"
 
-    if (!"Candidato" %in% names(this_dat)) {
-      colnames(this_dat) <- c("Ente", "Liste.Gruppi", "Voti", "Candidato", "X")
-      this_dat$X <- NULL
-      this_dat$Candidato <- NA
-    } else {
-      this_dat <- this_dat[,1:4]
-      colnames(this_dat) <- c("Ente", "Candidato", "Liste.Gruppi", "Voti")
-      this_dat$Candidato <- as.character(this_dat$Candidato)
-      this_dat$Candidato[this_dat$Candidato == ""] <- NA
-      this_dat$Candidato <- na.locf(this_dat$Candidato)
-    }
-    this_dat$Voti <- as.numeric(gsub("\\.", "", this_dat$Voti))
-    return(makeDF(file_details,
-                   data.frame(geo_entity = this_dat$Ente,
-                              candidate = this_dat$Candidato,
-                              party_votes = this_dat$Voti,
-                              party = this_dat$Liste.Gruppi,
-                              stringsAsFactors = F)))
-  }
-}
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "OLBIA-TEMPIO" &
+                               comune_minint_2006$original == "SAN TEODORO"] <- "104023"
 
-require(data.table)
-scrutini_dat <- rbindlist(lapply(files[grepl("SCRUTINI", files)], read_dat))
-liste_dat <- rbindlist(lapply(files[grepl("LISTE",files)], read_dat))
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "PESARO E URBINO" &
+                               comune_minint_2006$original == "PEGLIO"] <- "041041"
 
-# Encoding
-Encoding(scrutini_dat$geo_entity) <- "UTF-8"
-scrutini_dat$geo_entity <-
-  gsub("\xc0", "A'", scrutini_dat$geo_entity)
-scrutini_dat$geo_entity <-
-  gsub("\xc8", "E'", scrutini_dat$geo_entity)
-scrutini_dat$geo_entity <-
-  gsub("\xd2", "O'", scrutini_dat$geo_entity)
-scrutini_dat$geo_entity <-
-  gsub("\xd9", "U'", scrutini_dat$geo_entity)
+comune_minint_2006$PRO_COM_T[comune_minint_2006$provincia == "BRESCIA" &
+                               comune_minint_2006$original == "BRIONE"] <- "017030"
 
-Encoding(liste_dat$geo_entity) <- "UTF-8"
-liste_dat$geo_entity <-
-  gsub("\xc0", "A'", liste_dat$geo_entity)
-liste_dat$geo_entity <-
-  gsub("\xc8", "E'", liste_dat$geo_entity)
-liste_dat$geo_entity <-
-  gsub("\xd2", "O'", liste_dat$geo_entity)
-liste_dat$geo_entity <-
-  gsub("\xd9", "U'", liste_dat$geo_entity)
 
-# Trim
-for (var in colnames(liste_dat)) {
-  if(is.numeric(liste_dat[[var]])) next
-  liste_dat[[var]] <- str_trim(liste_dat[[var]])
-}
-for (var in colnames(scrutini_dat)) {
-  if(is.numeric(scrutini_dat[[var]])) next
-  scrutini_dat[[var]] <- str_trim(scrutini_dat[[var]])
-}
+camera_italia.20060409 <-
+  merge(camera_italia.20060409, 
+        comune_minint_2006,
+        by.x = c("PROVINCIA", "COMUNE"),
+        by.y = c("provincia", "original"))
 
-write.csv(scrutini_dat[scrutini_dat$election == 'Senato'],
-          file = "open_csv_20060409_senato_geo_count.csv",
-          row.names = F)
-write.csv(scrutini_dat[scrutini_dat$election == 'Camera'],
+
+camera_italia.20060409$VOTI_LISTA_num <- 
+  as.numeric(gsub(",00", "", camera_italia.20060409$VOTI_LISTA))
+
+open_csv_20060409_camera_geo_count <- 
+  camera_italia.20060409 %>%
+  dplyr::mutate(election = "Camera",
+                date = "2006-04-09",
+                geo_lev_1 = CIRCOSCRIZIONE,
+                geo_lev_2 = PROVINCIA,
+                geo_entity = COMUNE,
+                istat_cod = PRO_COM_T,
+                registered_voters = as.numeric(ELETTORI),
+                registered_male_voters = as.numeric(ELETTORI_MASCHI),
+                effective_voters =  as.numeric(VOTANTI),
+                effective_male_voters = as.numeric(VOTANTI_MASCHI),
+                blank_votes =  as.numeric(SCHEDE_BIANCHE),
+                VOTI_LISTA = VOTI_LISTA_num) %>%
+  dplyr::group_by(election, date, geo_lev_1, geo_lev_2, geo_entity,
+                  istat_cod, 
+                  registered_voters, registered_male_voters, 
+                  effective_voters, effective_male_voters,
+                  blank_votes) %>%
+  dplyr::summarise(tot_party_votes = sum(VOTI_LISTA))
+
+open_csv_20060409_camera_party_count <- 
+  camera_italia.20060409 %>%
+  dplyr::mutate(election = "Camera",
+                date = "2006-04-09",
+                geo_lev_1 = CIRCOSCRIZIONE,
+                geo_lev_2 = PROVINCIA,
+                geo_entity = COMUNE,
+                istat_cod = PRO_COM_T,
+                party = LISTA,
+                party_votes = VOTI_LISTA_num) %>%
+  dplyr::select(election:party_votes)
+
+write.csv(open_csv_20060409_camera_geo_count, 
           file = "open_csv_20060409_camera_geo_count.csv",
           row.names = F)
-write.csv(liste_dat[liste_dat$election == 'Senato'],
-          file = "open_csv_20060409_senato_party_count.csv",
-          row.names = F)
-write.csv(liste_dat[liste_dat$election == 'Camera'],
+
+write.csv(open_csv_20060409_camera_party_count, 
           file = "open_csv_20060409_camera_party_count.csv",
           row.names = F)
-
-
-
-
